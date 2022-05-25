e80d05e29f451ea6b85874bce7de64164b8ded33
Minh Hoàng IT <chuminhhoang19@gmail.com>
2022-05-25 22:17:16 +0700

    create project base

---
+++ src/routes/router.js
@@ -0,0 +1,9 @@
+import Home from "~/pages/Home/home.component";
+
+const publicRoutes = [
+    { path: '/', component: Home },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
